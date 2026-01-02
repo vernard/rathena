fb9045c9e0339ae9fea5b1cf9e2b66b7bec5d5b1
Vernard Alexis Luz <vernard@luzcommerce.com>
2026-01-03 05:38:16 +0800

    Add custom defines_pre.hpp

---
+++ src/config/defines_pre.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) rAthena Dev Teams - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+
+#ifndef CONFIG_CUSTOM_DEFINES_PRE_HPP
+#define CONFIG_CUSTOM_DEFINES_PRE_HPP
+
+/**
+ * Custom PACKETVER for Reminisce RO
+ * Must match the client's packet version
+ **/
+
+#define PACKETVER 20190605
+
+#endif /* CONFIG_CUSTOM_DEFINES_PRE_HPP */
